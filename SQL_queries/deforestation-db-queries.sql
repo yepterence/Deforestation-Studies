73fbbe341f62b08599da51ea8d52aafd4474439c
yepterence <terence.yep@gmail.com>
2020-10-02 00:41:40 -0400

    Rewrote create view query which includes dealing with NULL values, converts sqmi to sqkm for values in total_area_sq_mi, and created new column that calculates percent decrease in forest area. Removed redundant queries. TODO: fix query that compares difference amount to country

--- SQL_queries/deforestation-db-queries.sql
+++ SQL_queries/deforestation-db-queries.sql
@@ -1,12 +1,14 @@
-CREATE VIEW forestation 
-AS
-SELECT fa.country_name country, fa.country_code country_code, fa.year year_recorded, fa.forest_area_sqkm forest_area, la.total_area_sq_mi*2.59 land_area, r.region region_name, r.income_group income_grp
-FROM forest_area fa
-JOIN land_area la
-ON fa.country_name = la.country_name AND fa.country_code = la.country_code
-JOIN regions r
-ON r.country_name = fa.country_name AND r.country_code = fa.country_code;
+Author: Terence Yep
 
+-- Create a view named forestation that combines all 3 tables in dataset
+CREATE VIEW forestation
+AS
+SELECT f.country_code, f.country_name, f.year, r.income_group, r.region, f.forest_area_sqkm, COALESCE(land_tab.total_land_area,0.01) total_land_area, f.forest_area_sqkm/total_land_area pct_forest_area
+FROM (SELECT country_code, year, country_name, (total_area_sq_mi * 2.59) total_land_area FROM land_area) land_tab
+JOIN forest_area f
+ON f.year = land_tab.year AND f.country_code = land_tab.country_code
+JOIN regions r
+ON land_tab.country_code = r.country_code;
 
 -- History of forest area(1990):
 
@@ -14,16 +16,9 @@ SELECT forest_area_sqkm
 FROM forestation 
 WHERE country_name = 'World'
 AND year = '1990';
+-- Assigning 2016 to year value in the WHERE clause will provide the value for 2016
 
--- 2016 forest area:
-
-SELECT forest_area_sqkm
-FROM forestation
-WHERE country_name = 'World'
-AND year = '2016';
-
-
--- Difference: 
+-- Change (in sq km) in the forest area of the world from 1990 to 2016: 
 SELECT ( a.forest_area_sqkm-b.forest_area_sqkm ) AS loss_sqkm,
  ( a.forest_area_sqkm-b.forest_area_sqkm ) * 100 / a.forest_area_sqkm
  loss_percent
@@ -35,21 +30,27 @@ WHERE a.year = 1990
  AND b.country_name = 'World'
 LIMIT 1;
 
--- Land area and value:
-SELECT * 
-FROM land_area
-WHERE total_area_sq_mi * 2.59 < 1324449
-AND year = 2016
-ORDER BY total_area_sq_mi DESC;
+-- TO-DO fix query in this section 
+-- compare the amount of forest area lost between 1990 and 2016, to which country's total area in 2016 is it closest to:
+SELECT ( a.forest_area_sqkm-b.forest_area_sqkm ) AS loss_sqkm,
+ ( a.forest_area_sqkm-b.forest_area_sqkm ) * 100 / a.forest_area_sqkm
+ loss_percent
+FROM forestation a,
+ forestation b
+WHERE a.year = 1990
+ AND b.year = 2016
+ AND a.country_name = 'World'
+ AND b.country_name = 'World'
+LIMIT 1;
 
 
--- percent forest area of largest forested area:
+-- percent change in forest area of the world between 1990 and 2016:
 SELECT year_recorded, country, region_name, (land_area-forest_area)/land_area *100 pct_forest_area
 FROM forestation 
 WHERE year_recorded = 2016 AND NOT country = 'World' 
 ORDER BY pct_forest_area DESC;
 
--- TO DO : Need to deal with NULL values in db
+
 
 -- REGIONAL OUTLOOK 
 -- pct total land area designated forested area:

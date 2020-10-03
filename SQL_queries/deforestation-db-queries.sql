4a5ef1803a64ab5d968d2c63118be09de439ff92
yepterence <terence.yep@gmail.com>
2020-10-03 10:45:10 -0400

    Calculated total percent land mass that is covered by forests in 2016 around the world

--- SQL_queries/deforestation-db-queries.sql
+++ SQL_queries/deforestation-db-queries.sql
@@ -1,4 +1,4 @@
-Author: Terence Yep
+-- Author: Terence Yep
 
 -- Create a view named forestation that combines all 3 tables in dataset
 CREATE VIEW forestation
@@ -10,6 +10,7 @@ ON f.year = land_tab.year AND f.country_code = land_tab.country_code
 JOIN regions r
 ON land_tab.country_code = r.country_code;
 
+-- Global Situation
 -- History of forest area(1990):
 
 SELECT forest_area_sqkm
@@ -18,7 +19,7 @@ WHERE country_name = 'World'
 AND year = '1990';
 -- Assigning 2016 to year value in the WHERE clause will provide the value for 2016
 
--- Change (in sq km) in the forest area of the world from 1990 to 2016: 
+-- Change (in sq km and percentage) in the forest area of the world from 1990 to 2016: 
 SELECT ( a.forest_area_sqkm-b.forest_area_sqkm ) AS loss_sqkm,
  ( a.forest_area_sqkm-b.forest_area_sqkm ) * 100 / a.forest_area_sqkm
  loss_percent
@@ -31,7 +32,7 @@ WHERE a.year = 1990
 LIMIT 1;
 
 
--- compare the amount of forest area lost between 1990 and 2016, to which country's total area in 2016 is it closest to:
+-- compare the amount of forest area lost between 1990 and 2016, and to which country's land mass is the area lost most similar to in 2016:
 WITH ar_lost AS (SELECT b.year , ( a.forest_area_sqkm-b.forest_area_sqkm ) AS loss_sqkm
 FROM forestation a,
  forestation b
@@ -48,21 +49,14 @@ WHERE f.total_land_area < ar_lost.loss_sqkm
 ORDER BY 3 DESC
 LIMIT 1;
 
--- percent change in forest area of the world between 1990 and 2016:
-SELECT year_recorded, country, region_name, (land_area-forest_area)/land_area *100 pct_forest_area
-FROM forestation 
-WHERE year_recorded = 2016 AND NOT country = 'World' 
-ORDER BY pct_forest_area DESC;
-
-
-
 -- REGIONAL OUTLOOK 
--- pct total land area designated forested area:
-
-SELECT year_recorded, country, land_area, region_name, (land_area-forest_area)/land_area *100 pct_forest_area
-FROM forestation 
-WHERE year_recorded = 2016 AND country = 'World' 
-ORDER BY pct_forest_area DESC;
+-- What was the percent forest of the entire world in 2016?
+SELECT (total_forest/total_land)*100 pct_forest_world
+FROM(SELECT f.year year_recorded,  SUM(forest_area_sqkm) total_forest, SUM(total_land_area) total_land
+FROM forestation f
+WHERE f.year = 2016 AND f.country_name = 'World' 
+GROUP BY 1)
+;
 
 
 SELECT year_recorded, country, land_area, region_name, (land_area-forest_area)/land_area *100 pct_forest_area
